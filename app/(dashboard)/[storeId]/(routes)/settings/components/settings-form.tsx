a5489a08caa6c72a22dde022573d227857ef8499
westonsam <132954237+westonsam@users.noreply.github.com>
2023-07-19 13:39:03 -0700

    Fixed Imports formatting on Settings Form

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -10,14 +10,21 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { toast } from "react-hot-toast";
 
+import { useOrigin } from "@/hooks/use-origin";
 import { Heading } from "@/components/ui/heading";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { AlertModal } from "@/components/modals/alert-modal";
 import { ApiAlert } from "@/components/ui/api-alert";
-import { useOrigin } from "@/hooks/use-origin";
 
 interface SettingsFormProps {
     initialData: Store;
@@ -35,7 +42,7 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({
     const params = useParams();
     const router = useRouter();
     const origin = useOrigin();
-    
+
     const [open, setOpen] = useState(false);
     const [loading, setLoading] = useState(false);
 
