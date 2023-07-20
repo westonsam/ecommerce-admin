69570cb3ea2810542e405dbaa630b5adfe7bee6b
westonsam <132954237+westonsam@users.noreply.github.com>
2023-07-20 14:26:40 -0700

    Developed Categories Page
    Form + Data Table + API calls

--- app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -1,8 +1,8 @@
 import prismadb from "@/lib/prismadb";
 import { format } from "date-fns";
 
-import { BillboardClient } from "./componenets/client";
-import { BillboardColumn } from "./componenets/columns";
+import { BillboardClient } from "./components/client";
+import { BillboardColumn } from "./components/columns";
 
 const BillboardsPage = async ({
     params
